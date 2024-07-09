e7ab0f1f8acdd3bed32af785092f29fa21be755b
MOJIb111 <anikin.yasha@yandex.ru>
2024-07-09 19:34:09 +0700

    ain

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-import json
 import time
 import requests
 from bs4 import BeautifulSoup as B
 from datetime import datetime
+import pickle
 
-news_dict = {}
+class Article:
+
+    @classmethod
+    def __init__(cls, title, desc, url, time, id):
+        cls.title = title
+        cls.desc = desc
+        cls.url = url
+        cls.time = time
+        cls.id = id
+    @classmethod
+    def __getstate__(cls) -> dict:
+        '''Метод для записи атрибутов класса в файл'''
+        state = {}
+        state['title'] = cls.title
+        state['desc'] = cls.desc
+        state['url'] = cls.url
+        state['time'] = cls.time
+        state['id'] = cls.id
+        return state
+    @classmethod
+    def __setstate__(cls, state:dict):
+        '''Метод для восстановления класса из байтов'''
+        cls.title = state['title']
+        cls.desc = state['desc']
+        cls.url = state['url']
+        cls.time = state['time']
+        cls.id = state['id']
+
 def get_first():
+    '''Собирает первую партию новостей в объект'''
     heads = {
         "User-Agent" :
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0"
@@ -18,29 +46,24 @@ def get_first():
     articles = soup.find_all("article", class_="tm-articles-list__item")
 
     for article in articles:
-        article_title = article.find("h2", class_="tm-title").text
-        article_desc = article.find(class_="tm-article-body").text
-        article_url = f'https://habr.com{article.find("h2").find("a").get("href")}'
+        Article.title = article.find("h2", class_="tm-title").text
+        Article.desc = article.find(class_="tm-article-body").text
+        Article.url = f'https://habr.com{article.find("h2").find("a").get("href")}'
 
         article_date_time = article.find("time").get("datetime")
         date_from_iso = datetime.fromisoformat(article_date_time)
         date_time = datetime.strftime(date_from_iso, "%Y-%m-%d %H:%M:%S")
-        article_date_timestamp = time.mktime(datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S").timetuple())
+        Article.time = time.mktime(datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S").timetuple())
 
-        article_id = article_url.split("/")
-        article_id = article_id[-2]
-
-        news_dict[article_id] = {
-            "article_date_timestamp" : article_date_timestamp,
-            "article_title" : article_title,
-            "article_desc" : article_desc,
-            "article_url" : article_url
-        }
-
-    with open("news_dict.json", "w", encoding="utf-8") as file:
-        json.dump(news_dict, file, indent=4, ensure_ascii=False)
-
-
+        Article.id = Article.url.split("/")
+        Article.id = Article.id[-2]
+        
+        a = Article.__getstate__()
+    
+        with open('News.pkl', 'wb') as fp:
+            pickle.dump(a, fp)
+            
+print(get_first())
 def check_news_upd():
     with open("news_dict.json", encoding="utf-8") as file:
         news_dict = json.load(file)
